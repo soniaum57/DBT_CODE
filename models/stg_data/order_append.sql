624692724da06beb31be39685d2b725cef27ef80
soniaum57@gmail.com <soniaum57@gmail.com>
2024-09-01 11:28:30 +0000

    commit

--- models/stg_data/order_append.sql
+++ models/stg_data/order_append.sql
@@ -3,4 +3,4 @@
     incremental_strategy="append",
 ) }}
 
-select * from {{source('datafeed_shared_schema','STG_ORDERS')}} where id in(97, 98, 99)
+select * from {{source('datafeed_shared_schema','STG_ORDERS')}}
